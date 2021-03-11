d1c63652b7d5f5628e18324d8d36ba2bc83882c8
Егор <egor_ars2000@mail.ru>
2021-03-11 18:07:07 +0300

    hello

---
+++ src/main/java/ru/geekbrains/lesson1/FirstApp.java
@@ -0,0 +1,43 @@
+package ru.geekbrains.lesson1;
+
+
+public class FirstApp {
+    public static void main(String [] args) {
+        System.out.println("Hello Egor");
+// задание 4
+        //5 задание не понял как делается ( пока полный чайник во всем этом)
+
+
+
+
+
+        }
+    static void  quiq( ){
+        boolean b = false;
+        if (b) {
+            System.out.println("First");
+        } else if (10 > 20) {
+            System.out.println("10 > 20");
+        } else if (2 < 10) {
+            System.out.println("2 < 10");
+            //задание 2
+        }
+
+
+
+    }
+    static void printer() {
+
+    }
+
+    {
+        int a = 10;
+        int b = 10;
+        int c = 6;
+        int d = 3;
+        int x = a * (b + ( c / d ));
+        System.out.println("x = " + x);
+        //задание 1
+
+    }
+}
